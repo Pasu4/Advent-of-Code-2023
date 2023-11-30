b1086ce7c169106f820c7373ea58d32c2a916bec
Pasu4 <pascalwieckhorst02@gmail.com>
2023-11-30 15:53:36 +0100

    Nevermind, I AM READING THE FILE

--- day01/Source.asm
+++ day01/Source.asm
@@ -1,37 +1,47 @@
-
 INCLUDELIB kernel32.lib
 ExitProcess PROTO
-; CreateFileA PROTO
-; ReadFile PROTO
+CreateFileA PROTO
+ReadFile PROTO
+GetLastError PROTO
 
 .DATA
-; filename BYTE "./input.txt", 0  ; input file name
-input BYTE 256 DUP (0)          ; buffer, temp size
-; handle HANDLE ?                 ; file handle
+file_name BYTE ".\input.txt", 0 ; file name
+file_handle QWORD ?             ; file handle
+buffer BYTE 256 DUP (0)          ; buffer, temp size
+_ QWORD ?                       ; discard
 
 .CODE
 main PROC
 
-; SUB RSP, 32     ; Windows
 
 ; open file
-; stack arguments, reverse order?
-; PUSH 0                          ; template file, ignored when opening files
-; PUSH 128                        ; file attribute: FILE_ATTRIBUTE_NORMAL
-; PUSH 3                          ; OPEN_EXISTING
-; MOV R9, 0                       ; no security attributes
-; MOV R8, 1                       ; share mode: FILE_SHARE_READ
-; MOV RDX, GENERIC_READ           ; read permission
-; LEA RCX, filename               ; name of file
-; CALL CreateFileA                ; open file
-; ADD RSP, 24                     ; free stack
-; 
-; MOV hfile, RAX                  ; save file handle
+; stack arguments, 3 QWORDs
+PUSH 0                          ; 7: template file, ignored when opening files (NULL)
+PUSH 128                        ; 6: file attribute: FILE_ATTRIBUTE_NORMAL
+PUSH 3                          ; 5: file mode: OPEN_EXISTING
+MOV R9, 0                       ; 4: no security attributes (NULL)
+MOV R8, 0                       ; 3: share mode: ???
+MOV RDX, 3                      ; 2: read permission
+LEA RCX, file_name              ; 1: name of file
+SUB RSP, 4*8                    ; reserve 4 QWORDs
+CALL CreateFileA                ; open file
+ADD RSP, 7*8                    ; free 7 QWORDs
+
+MOV file_handle, RAX            ; save file handle
 
 ; Read from file
+; stack argument, 1 QWORD
+PUSH 0                          ; 5: overlap flag (NULL)
+LEA R9, _                       ; 4: actual length read (discard)
+MOV R8, LENGTHOF buffer         ; 3: max number of bytes to read
+LEA RDX, buffer                 ; 2: pointer to buffer
+MOV RCX, file_handle            ; 1: file handle
+SUB RSP, 32                     ; reserve 4 QWORDs
+CALL ReadFile                   ; read file
+ADD RSP, 5*8                    ; free 5 QWORDS
 
 
-; ADD RSP, 32     ; /Windows
+CALL GetLastError
 
 CALL ExitProcess
 main ENDP
