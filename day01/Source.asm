ede8f29ca8558268c0b83f98ad5b88365ad5799b
Pasu4 <pascalwieckhorst02@gmail.com>
2023-11-30 17:15:20 +0100

    Output to console for good measure

--- day01/Source.asm
+++ day01/Source.asm
@@ -3,16 +3,29 @@ ExitProcess PROTO
 CreateFileA PROTO
 ReadFile PROTO
 GetLastError PROTO
+GetStdHandle PROTO
+WriteConsoleA PROTO
 
 .DATA
 file_name BYTE ".\input.txt", 0 ; file name
 file_handle QWORD ?             ; file handle
+stdout QWORD ?                  ; output handle
 buffer BYTE 256 DUP (0)         ; buffer, temp size
+len QWORD ?                     ; length read from file
 _ QWORD ?                       ; discard
+
+; Output texts
+err_file BYTE "Could not read file", 0    ; file read error message
 
 .CODE
 main PROC
 
+; Get console handle
+MOV RCX, -11                    ; 1: output device code
+SUB RSP, 4*8                    ; reserve 4 QWORDs
+CALL GetStdHandle               ; get output handle
+ADD RSP, 4*8                    ; free 4 QWORDs
+MOV stdout, RAX                 ; save output handle
 
 ; open file
 ; stack arguments, 3 QWORDs
@@ -25,25 +38,52 @@ MOV RDX, 3                      ; 2: read permission
 LEA RCX, file_name              ; 1: name of file
 SUB RSP, 4*8                    ; reserve 4 QWORDs
 CALL CreateFileA                ; open file
-ADD RSP, 7*8                    ; free 7 QWORDs
-
 MOV file_handle, RAX            ; save file handle
+
+CALL GetLastError               ; check if there was an error
+ADD RSP, 7*8                    ; free 7 QWORDs
+CMP RAX, 0                      ; compare to 0 (OK)
+JNE file_error                  ; jump if there was an error
 
 ; Read from file
 ; stack argument, 1 QWORD
 PUSH 0                          ; 5: overlap flag (NULL)
-LEA R9, _                       ; 4: actual length read (discard)
+LEA R9, len                     ; 4: actual length read
 MOV R8, LENGTHOF buffer         ; 3: max number of bytes to read
 LEA RDX, buffer                 ; 2: pointer to buffer
 MOV RCX, file_handle            ; 1: file handle
 SUB RSP, 32                     ; reserve 4 QWORDs
 CALL ReadFile                   ; read file
-ADD RSP, 5*8                    ; free 5 QWORDS
 
+CALL GetLastError               ; check if there was an error
+ADD RSP, 7*8                    ; free 7 QWORDs
+CMP RAX, 0                      ; compare to 0 (OK)
+JNE file_error                  ; jump if there was an error
 
-CALL GetLastError
+; Print to console
+LEA R9, _                       ; 4: discard actual length written
+MOV R8, len                     ; 3: print as many characters as were read from the file
+LEA RDX, buffer                 ; 2: pointer to buffer to write
+MOV RCX, stdout                 ; 1: output handle
+SUB RSP, 4*8                    ; reserve 4 QWORDs
+CALL WriteConsoleA              ; get output handle
+ADD RSP, 4*8                    ; free 4 QWORDs
 
 CALL ExitProcess
+
+; Handle file read error
+file_error:
+    ; Output to console
+    LEA R9, _                       ; 4: discard actual length written
+    MOV R8, LENGTHOF err_file       ; 3: print as many characters as were read from the file
+    LEA RDX, err_file               ; 2: pointer to error message
+    MOV RCX, stdout                 ; 1: output handle
+    SUB RSP, 4*8                    ; reserve 4 QWORDs
+    CALL WriteConsoleA              ; get output handle
+    ADD RSP, 4*8                    ; free 4 QWORDs
+
+    CALL ExitProcess
+
 main ENDP
 
 END
